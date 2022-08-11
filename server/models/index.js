78470862980d717f11a72277ce9599c2bcf4cc7e
leehj24 <hyunji4579@gmail.com>
2022-08-11 13:48:38 +0900

    [feat] login,회원가입 api 작성
    passport 라이브러리를 활용한 서비스 인증 로직 구현
    
    resolve : #40 #41

--- server/models/index.js
+++ server/models/index.js
@@ -5,8 +5,10 @@ const config = require('../config/config.json')[env];
 const db = {};
 
 const sequelize = new Sequelize(config.database, config.username, config.password, config);
+
 db.User = require('./users')(sequelize, Sequelize);
-db.User = require('./usermanage')(sequelize, Sequelize);
+
+/*db.User = require('./usermanage')(sequelize, Sequelize);
 db.User = require('./email')(sequelize, Sequelize);
 db.User = require('./delete')(sequelize, Sequelize);
 db.User = require('./restore')(sequelize, Sequelize);
@@ -16,6 +18,7 @@ db.User = require('./first_email')(sequelize, Sequelize);
 db.User = require('./add_email')(sequelize, Sequelize);
 db.User = require('./add2_email')(sequelize, Sequelize);
 db.User = require('./reward')(sequelize, Sequelize);
+*/
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
--- server/passport/local_strategy.js
+++ server/passport/local_strategy.js
@@ -1,23 +1,24 @@
 const bcrypt = require('bcrypt');
-const db = require('../application/db/user');
+const db = require('../services/db/user');
 
 const localConfig = { usernameField: 'id', passwordField: 'password' };
 
-const localVerify = async(id, password, done) => {
-    try{
-        const user = await db.selectUser(id);
-        if(!user){
-            return done(null, false);
-        }
-        const compareResult = await bcrypt.compare(password, user.password);
-        if(!compareResult){
-            return done(null, false);
-        }
-        return done(null, user);
-    } catch(error){
-        console.error(error);
-        return done(null);
+const localVerify = async (id, password, done) => {
+  try {
+    console.log ('debug test')
+    const user = await db.selectUserFromPassport(id);
+   
+    if (!user) {
+      return done(null, false);
     }
+    const compareResult = await bcrypt.compare(password, user.password);
+    if (!compareResult) {
+      return done(null, false);
+    }
+    return done(null, user);
+  } catch (error) {
+    return done(null);
+  }
 };
 
 module.exports = { localConfig, localVerify };
