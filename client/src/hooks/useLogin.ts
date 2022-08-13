56593228b05267899a42e04d95d45d13a4cdc4c4
Hyuntak Lee <ttttaakk620@gmail.com>
2022-08-13 19:46:15 +0900

    [feat] 사용자 인증 상태 관리 구현
    ContextAPI를 사용해 회원 데이터를 전역 상태로 관리

--- client/src/hooks/useLogin.ts
+++ client/src/hooks/useLogin.ts
@@ -1,9 +1,18 @@
 import { useMutation } from "@tanstack/react-query";
 import {login} from '../api/auth';
 
+import { useNavigation } from "@react-navigation/core";
+import { useUserState } from "../contexts/UserContext";
+import { RootStackNavigationProp } from "../stacks/types";
+
 export default function useLogin() {
+    const [, setUser] = useUserState();
+    const navigation = useNavigation<RootStackNavigationProp>();
+
     const mutation = useMutation(login, {
         onSuccess: (data) => {
+            setUser(data.user);
+            navigation.pop();
             console.log(data);
         },
         onError: (error) => {
