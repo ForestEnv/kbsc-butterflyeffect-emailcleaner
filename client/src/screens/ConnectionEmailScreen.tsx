f3493511051122792ae0598c847ef3b84295cb35
Hyuntak Lee <ttttaakk620@gmail.com>
2022-08-16 08:16:40 +0900

    [feat] 로그인 이후 이메일 연동 실행
    rootstack에서 스크린 이동 시 context에 저장된 user가 아닌, 서버에서 받은 isConnectionEmail로 분기함

--- client/src/screens/ConnectionEmailScreen.tsx
+++ client/src/screens/ConnectionEmailScreen.tsx
@@ -1,20 +1,34 @@
-import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, TextInput } from 'react-native'
 
 import { useNavigation } from '@react-navigation/core';
 //import { RootStackNavigationProp } from '../stacks/types';
 import { MainTabNavigationProp } from '../stacks/types';
 import { useUserState } from "../contexts/UserContext";
 
+import useConnection from '../hooks/useConnection';
+
 function ConnectionEmailScreen() {
   const navigation = useNavigation<MainTabNavigationProp>();
   const [user, setUser] = useUserState();
-  console.log('사용자 정보:', user);
+  const [email, setEmail] = useState('');
+
+  const {mutate: emailConnction} = useConnection();
+  
+  //타입 오류 : connection의 id 타입을 유니온 타입으로 대응
+  const onConnectionSubmit = () => {
+    emailConnction({
+      id:user?.id,
+      email,
+    });
+  }
+
   return (
     <View>
-      <Text>ConnectionEmailScreen</Text>
-      <TouchableOpacity onPress={() => navigation.navigate('MainTab', {screen: 'Home'})}>
-        <Text>홈으로 이동</Text>
+      <Text>이메일 연동하기</Text>
+      <TextInput style={{borderWidth:2}}value={email} onChangeText={setEmail} placeholder="연동할 이메일 주소를 입력하세요"/>
+      <TouchableOpacity style={{alignItems:'center',backgroundColor:'grey', marginTop:10, borderWidth:2}}onPress={onConnectionSubmit}>
+        <Text>추가</Text>
       </TouchableOpacity>
     </View>
   )
--- client/src/screens/HomeScreen.tsx
+++ client/src/screens/HomeScreen.tsx
@@ -5,12 +5,15 @@ import authStorage from '../storages/authStorage';
 
 const HomeScreen = () => {
   const [user, setUser] = useUserState();
-  console.log('사용자 정보:', user);
+  console.log('사용자 정보:', user?.id);
 
   //asyncstorage정보 확인 debug
   useEffect(() => {
     const fn = async () => {
-      console.log('스토리지:', await authStorage.get())
+      //console.log('스토리지:', await authStorage.get())
+      const test = await authStorage.get()
+      const temp = test?.user.isConnectionEmail;
+      console.log('테스트:', temp);
     }
     fn();
   },[]);
