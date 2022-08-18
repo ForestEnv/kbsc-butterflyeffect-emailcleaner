f263c80a8a9787460c0e18ff8ca666626ce106dc
Hyuntak Lee <ttttaakk620@gmail.com>
2022-08-18 15:14:14 +0900

    [feat] user, email 테이블 관계 정의

--- server/models/index.js
+++ server/models/index.js
@@ -6,7 +6,13 @@ const db = {};
 
 const sequelize = new Sequelize(config.database, config.username, config.password, config);
 
+//데이터베이스 모델 정의
 db.User = require('./users')(sequelize, Sequelize);
+db.Email = require('./email')(sequelize, Sequelize);
+
+//데이터베이스 관계 정의
+db.User.hasMany(db.Email, {foreignKey:'no'});
+db.Email.belongsTo(db.User, {foreignKey:'user_no'});
 
 /*db.User = require('./usermanage')(sequelize, Sequelize);
 db.User = require('./email')(sequelize, Sequelize);
