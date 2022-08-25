9f343bc9aafc6454f1786f4b12142360dd7e42da
leehj24 <hyunji4579@gmail.com>
2022-08-25 14:04:22 +0900

    feat deletelink
    이메일 삭제 구현/db저장
    
    solve: #109

--- server/services/db/delete.js
+++ server/services/db/delete.js
@@ -1,14 +1,10 @@
 //const { Email } = require("../../models");
 const { Delete } = require("../../models");
-//const { hashingPw } = require("../../utils/bcrypt");
 
-exports.setDeleteEmails = async ({ Emails }) => {
-  console.log("************");
-  console.log(Emails);
-  console.log("************");
-  await Delete.bulkCreate(Emails).then(() => {
-    console.log("************11");
-    return "Delete.findAll()";
-  });
+exports.setDeleteEmails = async (Emails) => {
   console.log("************11");
-};
+  console.log(Emails);
+  console.log("************22");
+  await Delete.bulkCreate(Emails);
+  console.log("************33");
+}
