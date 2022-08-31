d7dcdb83ac61245e78a99a376e46633a823bd7c1
huiyeol <huiyy9211@gmail.com>
2022-08-31 15:02:15 +0900

    feat: reward
    보상 부분 랭크기능 추가

--- server/routes/reward/index.js
+++ server/routes/reward/index.js
@@ -3,5 +3,6 @@ const middlewares = require("../../middlewares/auth");
 const controller = require("./controller");
 
 router.get("/getStatistics/:userNo", controller.showStatistics);
+router.get("/getRank/:userNo", controller.showRank);
 
 module.exports = router;
